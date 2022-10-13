b26b70d27ca92c21b0c8b7e6e368ba7337db01a0
LyanMoraless <drlyanmorales@gmail.com>
2022-10-13 11:45:13 -0300

    Crud sem postgres

--- crud-back/app.py
+++ crud-back/app.py
@@ -4,11 +4,6 @@ from flask_restful import Api
 from resources.player import Players, Player
 
 app = Flask(__name__)
-
-@app.route('/players', methods=['GET'])
-def get(self):
-    return {'Players': players}
-
 api = Api(app)
 
 api.add_resource(Players, '/players')
