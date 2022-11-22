3976dbfc6498f474f6c8a28a492ebaa0d30d4c4d
LyanMoraless <drlyanmorales@gmail.com>
2022-11-22 12:52:02 -0300

    Funcionando

---
+++ crud-back/factory.py
@@ -0,0 +1,29 @@
+import psycopg2 
+
+class Factory:
+  conn = psycopg2.connect("dbname=CrudHcosta user=postgres password=123456")
+  cur = conn.cursor()
+
+  def create(self):
+    self.cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+    self.conn.commit()
+    self.close_connections()
+
+  def insert(self):
+    self.cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+    self.conn.commit()
+    self.close_connections()
+
+
+  def select(self):
+    self.cur.execute("SELECT * FROM test;")
+    data = self.cur.fetchone()
+    self.conn.commit()
+    print(data)
+    self.close_connections()
+
+  def close_connections(self):
+    self.cur.close()
+    self.conn.close()
+
+Factory().select()
